a4f8f5406fbb28f302c9cf63a9999a58572fc2fe
Yeiker Daniel <84932338+Drizzysavage@users.noreply.github.com>
2024-06-11 15:41:08 -0400

    Update ScriptChallenger.py
    Other version

--- ScriptChallenger.py
+++ ScriptChallenger.py
@@ -1,23 +1,14 @@
-def sieve_of_eratosthenes(n):
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False
-
+def sieve(n):
+    primes = [False, False] + [True] * (n - 1)
     for i in range(2, int(n**0.5) + 1):
-        if is_prime[i]:
-            for j in range(i*i, n + 1, i):
-                is_prime[j] = False
-
-    prime_numbers = [num for num in range(n + 1) if is_prime[num]]
-    return prime_numbers
+        if primes[i]:
+            primes[i*i:n+1:i] = [False] * len(primes[i*i:n+1:i])
+    return [x for x in range(n + 1) if primes[x]]
 
 def main():
     n = 250
-    prime_numbers = sieve_of_eratosthenes(n)
-
     with open("results.txt", "w") as file:
-        for prime in prime_numbers:
-            file.write(f"{prime}\n")
-
+        file.write('\n'.join(map(str, sieve(n))))
     print("Prime numbers generated and saved in results.txt")
 
 if __name__ == "__main__":
